66bbecf5619982978767933af5fcb9671d832b6b
Rodrigo Lopes <rocostalopes@gmail.com>
2020-02-16 01:38:58 -0500

    Updates

---
+++ precache-manifest.8f4bc86c9f2acacb7855a302ef147669.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4857e31a609d6e1424cf4ebdbf70b6aa",
+    "url": "/hackernewsapi/index.html"
+  },
+  {
+    "revision": "df48c0ec1deb90212bfd",
+    "url": "/hackernewsapi/static/js/2.b43a2fba.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/hackernewsapi/static/js/2.b43a2fba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4870bac543e17f01a82a",
+    "url": "/hackernewsapi/static/js/main.c4734afe.chunk.js"
+  },
+  {
+    "revision": "91355dd0ec320a07fcbe",
+    "url": "/hackernewsapi/static/js/runtime-main.e6431776.js"
+  }
+]);
